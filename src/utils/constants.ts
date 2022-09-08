8850335890bc0553559642f76fd26251a76f47b4
Willians Faria <willians.faria@goflux.com.br>
2022-09-08 17:50:29 -0300

    feat: verifying if user exists before registering a new user

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,3 +3,4 @@ import jwt from 'jsonwebtoken'
 export const JWT_SECRET = process.env.JWT_SECRET as jwt.Secret
 export const JWT_ALGORITHM = process.env.JWT_ALGORITHM as jwt.Algorithm
 export const JWT_EXPIRE_TIMESTAMP = process.env.JWT_EXPIRE_TIMESTAMP as string | number
+export const DATABASE_URI = process.env.DATABASE_URI as string
